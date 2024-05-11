b19b9d87e84ebe38a768f61f1e2649053c240000
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-05-11 11:28:05 -0300

    feat: Criar reset css

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalFonts } from "./fonts/fonts.ts";
+import { GlobalStyle } from "./styles/global.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <GlobalFonts />
     <App />
   </React.StrictMode>
